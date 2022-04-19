74e8912005ee8c27924dfb1042d747a015823304
ttorbeynS <ttorbeyn@student.s19.be>
2022-04-19 11:45:42 +0200

    save

---
+++ includes/philo.h
@@ -0,0 +1,19 @@
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <string.h>
+# include <sys/wait.h>
+
+# include <pthread.h>
+
+typedef struct s_data
+{
+
+}				t_data;
+
+#endif
---
+++ philo.c
@@ -0,0 +1,8 @@
+#include "includes/philo.h"
+
+int	main(int ac, char **av)
+{
+	pthread_t	t1;
+	pthread_create()
+	return (0);
+}
