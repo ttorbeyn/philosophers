3bbb533514212ba725bf4089ac2d7f962736e07f
ttorbeynS <ttorbeyn@student.s19.be>
2022-04-21 20:58:44 +0200

    save

--- includes/philo.h
+++ includes/philo.h
@@ -19,22 +19,30 @@
 
 typedef struct s_philo
 {
-	int	id;
-	int fork_id;
-	int	state;
+	int			id;
+	int 		right_fork;
+	int 		left_fork;
+	long long	last_time_eat;
+	int			state;
 }				t_philo;
 
 typedef struct s_data
 {
-	int		nb_of_philo;
-	int		t_to_die;
-	int		t_to_eat;
-	int 	t_to_sleep;
-	int		nb_of_t_each_philo_must_eat;
-	t_philo	*philo;
+	int				nb_of_philo;
+	int				t_to_die;
+	int				t_to_eat;
+	int 			t_to_sleep;
+	int				nb_of_t_each_philo_must_eat;
+	long long		timestamp_start;
+	t_philo			*philo;
+	pthread_t		*philo_thread;
+	pthread_mutex_t	*fork;
+	pthread_mutex_t	write;
 }				t_data;
 
 
+//philo.c
+long get_timestamp(t_data *data);
 
 //ft_error.c
 int	ft_exit(char *error);
--- philo.c
+++ philo.c
@@ -2,7 +2,8 @@
 
 void*	function(void *arg)
 {
-//	printf("thread 1\n");
+	printf("thread 1\n");
+
 	if (!arg)
 		return (0);
 	return (0);
@@ -11,17 +12,25 @@ void*	function(void *arg)
 int philo_set(t_data *data)
 {
 	int	i;
-	int x;
 
-	x = 0;
-	i = 1;
+	i = 0;
 	while (i < data->nb_of_philo)
-		data->philo[x++].id = i++;
+	{
+		data->philo[i].id = i;
+		data->philo[i].right_fork = i;
+		data->philo[i].left_fork = (i + 1) % data->nb_of_philo;
+		data->philo[i].last_time_eat = data->timestamp_start;
+	}
 	return (0);
 }
 
+
+
+
 int	data_set(t_data *data, int ac, char **av)
 {
+	struct timeval	start;
+
 	data->nb_of_philo = ft_atoi(av[1]);
 	data->t_to_die = ft_atoi(av[2]);
 	data->t_to_eat = ft_atoi(av[3]);
@@ -31,33 +40,71 @@ int	data_set(t_data *data, int ac, char **av)
 	data->philo = malloc(sizeof(t_philo) * data->nb_of_philo);
 	if (!data->philo)
 		ft_exit("Malloc error");
+	gettimeofday(&start, NULL);
+	data->timestamp_start = start.tv_sec * 1000000 + start.tv_usec;
 	return (0);
 }
 
-long get_duration_process()
+long get_timestamp(t_data *data)
 {
-	struct timeval	old;
 	struct timeval	new;
 	long time;
 
-	gettimeofday(&old, NULL);
 //	printf("%ld\n", (time_base.tv_sec * 1000000 + time_base.tv_usec));
-	usleep(50);
 	gettimeofday(&new, NULL);
 //	printf("%ld\n", (time_new.tv_sec * 1000000 + time_new.tv_usec));
-	time = (new.tv_sec * 1000000 + new.tv_usec) - (old.tv_sec * 1000000 + old.tv_usec);
+	time = (new.tv_sec * 1000000 + new.tv_usec) - data->timestamp_start;
 //	printf("%ld\n", time);
 	return (time);
 }
+
+int init_mutex(t_data *data)
+{
+	int i;
+
+	i = 0;
+	data->fork = malloc(sizeof(pthread_mutex_t) * data->nb_of_philo);
+	if (!data->fork)
+		ft_exit("Malloc error");
+	while (i < data->nb_of_philo)
+	{
+		if (pthread_mutex_init(&data->fork[i], NULL))
+			ft_exit("Mutex error");
+		i++;
+	}
+	if (pthread_mutex_init(&data->write, NULL))
+		ft_exit("Mutex error");
+	return (0);
+}
+
 
 int philo(t_data *data)
 {
 	int i;
 
-	i = data->nb_of_philo;
-	pthread_t	philo1;
-	pthread_create(&philo1, NULL, function, NULL);
-	pthread_join(philo1, NULL);
+	i = 0;
+	data->philo_thread = malloc(sizeof(pthread_t) * data->nb_of_philo);
+	if (!data->philo_thread)
+		ft_exit("Malloc error");
+
+
+	while (i < data->nb_of_philo)
+	{
+		pthread_create(&data->philo_thread[i], NULL, function, NULL);
+		i++;
+	}
+	i = 0;
+	while (i < data->nb_of_philo)
+	{
+		pthread_mutex_init(&data->fork[i], NULL);
+		i++;
+	}
+	i = 0;
+	while (i < data->nb_of_philo)
+	{
+		pthread_join(data->philo_thread[i], NULL);
+		i++;
+	}
 	return (0);
 }
 
@@ -67,7 +114,7 @@ int	main(int ac, char **av)
 
 	if (ac != 5 && ac != 6)
 		ft_exit("Not the right number of arguments");
-	get_duration_process();
+	get_timestamp(&data);
 	data_set(&data, ac, av);
 	philo(&data);
 	return (0);
